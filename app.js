1f3b2123ddb7b6d3b6bc23f257d2be6c5e16b67f
sana.cheffi.stg <sana.cheffi.stg@sifast.com>
2025-03-19 14:30:19 +0100

    version 5

--- app.js
+++ app.js
@@ -10,13 +10,14 @@ const chatbotRouter=require("./routes/chatbot.route")
 const userRouter =require("./routes/user.route")
 const chatbotRequeteRouter = require("./routes/chatbot-requete.route")
 const paymentRouter =require("./routes/payment.route");
+const path = require('path'); // Ajout de l'importation de path
 app.use(express.json())
 app.use(cors())
 dotenv.config()
 
-app.get('/',(req,res) =>{
-    res.send("binevenue dans notre site")
-})
+// app.get('/',(req,res) =>{
+//     res.send("binevenue dans notre site")
+// })
 
 mongoose.connect(process.env.DATABASECLOUD)
 .then(()=>{console.log("connexion a la base de donnees est reussie")})
@@ -31,6 +32,11 @@ app.use("/api/chat",chatbotRouter);
 app.use('/api/users', userRouter);
 app.use('/api/chatbot', chatbotRequeteRouter);
 app.use('/api/payment', paymentRouter);
+
+//dist reactjs
+app.use(express.static(path.join(__dirname, './client/build'))); // Route pour les pages non trouvées, redirige vers index.html
+app.get('*', (req, res) => { res.sendFile(path.join(__dirname,'./client/build/index.html')); });
+
 app.listen(process.env.PORT,function(){
     console.log(`serveur is listen on port ${process.env.PORT}`)
 })
